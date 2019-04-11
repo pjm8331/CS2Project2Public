4b18ab7da1bc4cac2da13f7be4e6928139483aee
jmb3471 <47160881+jmb3471@users.noreply.github.com>
2019-04-11 18:20:40 -0400

    Renamed classes

--- src/client/gui/WAMBoard.java
+++ src/client/gui/WAMBoard.java
@@ -6,12 +6,12 @@ import java.util.List;
  * @author John Baxley(jnb3471)
  * @author Peter Mastropaolo(pjm8331)
  */
-public class WhackBoard {
+public class WAMBoard {
     public static int ROWS;
     public static int COLS;
     public static int TIME;
     public static int PLAYERS;
-    private List<Observer<WhackBoard>> observerlist;
+    private List<Observer<WAMBoard>> observerlist;
     private Spot[][] board;
 
     public enum Spot{
@@ -23,12 +23,13 @@ public class WhackBoard {
     public int getTimeLeft(){
         return 0;
     }
+
     public boolean isValid(int col, int row){
         return(col >= 0 && col < COLS) && (row >=0 && row < ROWS) &&
                 (this.board[col][row] == Spot.DOWN);
     }
 
-    public void addObserver(Observer<WhackBoard> observer){
+    public void addObserver(Observer<WAMBoard> observer){
         this.observerlist.add(observer);
     }
 }
