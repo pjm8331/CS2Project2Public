863c488388494c3742ea54c19b84a94a1c354ef6
pjm8331 <petermastropaolo@yahoo.com>
2019-04-11 18:10:36 -0400

    Merge branch 'master' of C:\Users\peter\Desktop\School\CS2\project-2-baxley-mastropaolo with conflicts.

--- src/client/gui/WhackBoard.java
+++ src/client/gui/WhackBoard.java
@@ -12,15 +12,23 @@ public class WhackBoard {
     public static int TIME;
     public static int PLAYERS;
     private List<Observer<WhackBoard>> observerlist;
+    private Spot[][] board;
 
+    public enum Spot{
+        UP, DOWN
+    }
     public enum Status{
         WIN, LOSE, TIE, ERROR, NOT_DONE
     }
     public int getTimeLeft(){
         return 0;
     }
+    public boolean isValid(int col, int row){
+        return(col >= 0 && col < COLS) && (row >=0 && row < ROWS) &&
+                (this.board[col][row] == Spot.DOWN);
+    }
 
     public void addObserver(Observer<WhackBoard> observer){
-        this.
+        this.observerlist.add()
     }
 }
