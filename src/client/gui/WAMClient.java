76486d3604aac35652f9658223c2c1489f4375e6
pjm8331 <petermastropaolo@yahoo.com>
2019-04-13 14:55:45 -0400

    started run switch statement

--- src/client/gui/WAMClient.java
+++ src/client/gui/WAMClient.java
@@ -3,7 +3,10 @@ package client.gui;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
+
+import static common.WAMProtocol.*;
 
 public class WAMClient {
     private Socket clientSocket;
@@ -29,6 +32,14 @@ public class WAMClient {
         catch (IOException e){}
     }
 
+    private synchronized boolean getGo(){
+        return this.go;
+    }
+
+    private synchronized void stop(){
+        this.go = false;
+    }
+
     public void startListener(){
         new Thread(() -> this.run()).start();
     }
@@ -43,7 +54,33 @@ public class WAMClient {
         this.wamBoard.close();
     }
 
+
     private void run(){
-        //todo
+        while(this.getGo()){
+            try{
+                String input = this.in.next();
+                String args = this.in.nextLine().trim();
+
+                switch(input){
+                    case WELCOME:
+                    case MOLE_UP:
+                    case MOLE_DOWN:
+                    case WHACK:
+                    case GAME_LOST:
+                    case GAME_TIED:
+                    case GAME_WON:
+                    case SCORE:
+                    case ERROR:
+                }
+            }
+            catch( NoSuchElementException e){
+                System.out.print("Lost connection: " + e);
+                this.stop();
+            }
+            catch( Exception f){
+                System.out.print(f.getMessage());
+                this.stop();
+            }
+        }
     }
 }
