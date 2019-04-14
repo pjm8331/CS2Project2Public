84afb486ecf5589fccf51fa0874e447a5ec98bed
jmb3471 <47160881+jmb3471@users.noreply.github.com>
2019-04-14 16:34:03 -0400

    added comments to observer

--- src/client/gui/Observer.java
+++ src/client/gui/Observer.java
@@ -1,6 +1,18 @@
 package client.gui;
 
-
+/**
+ * An interface representing any class whose objects get notified when
+ * the objects they are observing update them.
+ *
+ * @author John Baxley(jmb3471)
+ * @author Peter Mastropaolo(pjm8331)
+ */
 public interface Observer< Subject > {
+
+    /**
+     * @param subject the object that wishes to inform this object
+     *                about something that has happened.
+     * Observer design pattern</a>
+     */
     void update(Subject subject);
 }
