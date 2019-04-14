195c0a37ea89cabb055e68dd7217e0cbe939bde0
pjm8331 <petermastropaolo@yahoo.com>
2019-04-14 14:05:17 -0400

    completed WAMException

--- src/client/gui/WAMException.java
+++ src/client/gui/WAMException.java
@@ -1,4 +1,16 @@
 package client.gui;
 
 public class WAMException extends Exception {
+
+    public WAMException(String in){
+        super(in);
+    }
+
+    public WAMException(Throwable throwable){
+        super(throwable);
+    }
+
+    public WAMException(String in, Throwable throwable){
+        super(in, throwable);
+    }
 }
