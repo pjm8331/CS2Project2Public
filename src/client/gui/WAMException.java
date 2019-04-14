33e14a0eb5a33ec98c46de6568831cd211ebc8aa
pjm8331 <petermastropaolo@yahoo.com>
2019-04-14 14:29:46 -0400

    Fully commented WAMException

--- src/client/gui/WAMException.java
+++ src/client/gui/WAMException.java
@@ -1,15 +1,33 @@
 package client.gui;
 
+/**
+ * Custom exception for all Whack A Mole classes for errors
+ *@author John Baxley(jmb3471)
+ *@author Peter Mastropaolo(pjm8331)
+ */
 public class WAMException extends Exception {
 
+    /**
+     * WAM exception with a message
+     * @param in message for the error
+     */
     public WAMException(String in){
         super(in);
     }
 
+    /**
+     * WAM exception with a throwable
+     * @param throwable the reason the error was thrown
+     */
     public WAMException(Throwable throwable){
         super(throwable);
     }
 
+    /**
+     * WAM exception with both a throwable and a message
+     * @param in message for the error
+     * @param throwable the reason the error was thrown
+     */
     public WAMException(String in, Throwable throwable){
         super(in, throwable);
     }
