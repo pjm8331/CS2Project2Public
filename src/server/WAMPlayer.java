041e3e84dd610d8125c9913a018bf63a6ec773b8
jmb3471 <47160881+jmb3471@users.noreply.github.com>
2019-04-25 17:34:11 -0400

    finished constructor for wamplayer

--- src/server/WAMPlayer.java
+++ src/server/WAMPlayer.java
@@ -1,10 +1,35 @@
 package server;
 
+import client.gui.WAMException;
 import common.WAMProtocol;
 
 import java.io.Closeable;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class WAMPlayer implements WAMProtocol, Closeable {
 
+    private Socket socket;
+
+    private Scanner scanner;
+
+    private PrintStream printStream;
+
+    public WAMPlayer(Socket socket) throws WAMException{
+        this.socket = socket;
+        try {
+            this.scanner = new Scanner(socket.getInputStream());
+            this.printStream = new PrintStream(socket.getOutputStream());
+        }
+        catch (IOException e){
+            throw new WAMException(e);
+        }
+    }
+
+    public void connect(){
+        printStream.println(CONNECT);
+    }
     public void close(){}
 }
