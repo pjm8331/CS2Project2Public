a1bf0e534542c0163c4f92edc27b83dcd066accf
pjm8331 <petermastropaolo@yahoo.com>
2019-04-25 17:32:59 -0400

    Comments added to the WAMServer

--- src/server/WAMServer.java
+++ src/server/WAMServer.java
@@ -7,14 +7,28 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * Handles the creation of the players, the game, and all sockets for communication
+ *@author John Baxley(jmb3471)
+ *@author Peter Mastropaolo(pjm8331)
+ */
 public class WAMServer implements WAMProtocol, Runnable {
 
-    private ServerSocket server;
-    private int players;
-    private int rows;
-    private int cols;
-    private int time;
+    private ServerSocket server; //Server socket for the game
+    private int players; //How many players are in the game
+    private int rows; //The number of rows for the board
+    private int cols; //The number of columns for the board
+    private int time; //The amount of time the game will run for
 
+    /**
+     * Constructor for the server
+     * @param port what port to create the server at
+     * @param rows how many rows the game will have
+     * @param cols how many columns the game will have
+     * @param players how many players the game will have
+     * @param time how long the game will run
+     * @throws WAMException custom exception for the game
+     */
     public WAMServer(int port, int rows, int cols, int players, int time) throws WAMException{
         try{
             server = new ServerSocket(port);
@@ -28,6 +42,11 @@ public class WAMServer implements WAMProtocol, Runnable {
         this.time = time;
     }
 
+    /**
+     * main function for the creation of the game
+     * @param args takes in the args when creating the game
+     * @throws WAMException
+     */
     public static void main(String[] args) throws WAMException{
         if(args.length != 5){
             System.out.println("Usage: java WAMServer <port> <rows> <columns> <player #> <duration>");
@@ -42,6 +61,9 @@ public class WAMServer implements WAMProtocol, Runnable {
         server.run();
     }
 
+    /**
+     * Runs the server and creates players, the game, and the sockets for the players
+     */
     @Override
     public void run(){
         try{
